b1bb055610b594d396db99157e7cce8e3ca10858
Azheem Rahman <azheem.rahman@gmail.com>
2025-06-10 12:32:19 +0800

    move db.py into db/ module

--- upload_service/db/db.py
+++ upload_service/db/db.py
@@ -1,8 +1,5 @@
 import os
 from psycopg2 import pool
-from dotenv import load_dotenv
-
-load_dotenv()
 
 DB_HOST = os.getenv("DB_HOST", "localhost")
 DB_PORT = os.getenv("DB_PORT", "5432")
@@ -11,9 +8,11 @@ DB_NAME = os.getenv("DB_NAME", "video_processing")
 connection_pool = pool.SimpleConnectionPool(
     minconn=1,
     maxconn=10,
-    host=DB_HOST,
-    port=DB_PORT,
-    dbname=DB_NAME,
+    host=os.getenv("POSTGRES_HOST", "postgres"),
+    port=5432,
+    user=os.getenv("POSTGRES_USER", "postgres"),
+    password=os.getenv("POSTGRES_PASSWORD", "password"),
+    database=os.getenv("POSTGRES_DB", "mydb")
 )
 
 def get_connection():
